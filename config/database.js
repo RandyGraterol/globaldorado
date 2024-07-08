03d853ce2bef477968d33fb46f4d45d70a1219ac
RandyGraterol <soapdelinger@gmail.com>
2024-07-08 18:38:31 +0200

    deleteFile

--- config/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('globaldorado','administrador','k_hoA!qQBM2k/@e]', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
-
-module.exports=sequelize;
